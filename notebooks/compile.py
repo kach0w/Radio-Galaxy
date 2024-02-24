4969cfd88cdfb46624c45862118ebfacae0e133f
kach0w <karsab343@gmail.com>
2024-02-24 15:27:01 -0800

    yoohoo

--- notebooks/compile.py
+++ notebooks/compile.py
@@ -1,62 +1,22 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torchsummary import summary
 import json
 import os
+from notebooks.MiraBest_Goof import MiraBest
 
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize([0.5],[0.5])])
 
-def get_train_data():
-    image_directory = r"../RadioGalaxyNET/train"
-    annotations_directory = r"../RadioGalaxyNET/annotations/train.json"
-    with open(annotations_directory) as json_data:
-        data = json.load(json_data)
-    annotations = data["annotations"] 
-    images = data["images"]
+trainset = MiraBest(root='./batches', train=True, download=True, transform=transform)  
+batch_size_train = 2
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size_train, shuffle=True, num_workers=2)
 
-    train_image_arr = []
-    train_segmentation = []
-    train_bbox = []
-    train_keypoints = []
-    train_label = []
-
-    for i in range(len(images)):
-        file_name = images[i]["file_name"]
-        image = cv.imread(os.path.join(image_directory, file_name))
-        segmentation = annotations[i]["segmentation"]
-        bbox = annotations[i]["bbox"]
-        keypoints = annotations[i]["keypoints"]
-        label = annotations[i]["category_id"]
-
-        train_image_arr.append(image)
-        train_segmentation.append(segmentation)
-        train_bbox.append(bbox)
-        train_keypoints.append(keypoints)
-        train_label.append(label)
-    return (train_image_arr, train_segmentation, train_bbox, train_keypoints, train_label);
-
-
-
-def get_test_data():
-    test_image_directory = r"../RadioGalaxyNET/test"
-    test_annotations_directory = r"../RadioGalaxyNET/annotations/test.json"
-    with open(test_annotations_directory) as json_data:
-        data = json.load(json_data)
-    test_annotations = data["annotations"] 
-    test_images = data["images"]
-    test_image_arr = []
-    test_segmentation = []
-    test_bbox = []
-    test_keypoints = []
-    test_label = []
-    for i in range(len(test_images)):
-        file_name = test_images[i]["file_name"]
-        image = cv.imread(os.path.join(test_image_directory, file_name))
-        segmentation = test_annotations[i]["segmentation"]
-        bbox = test_annotations[i]["bbox"]
-        keypoints = test_annotations[i]["keypoints"]
-        label = test_annotations[i]["category_id"]
-
-        test_image_arr.append(image)
-        test_segmentation.append(segmentation)
-        test_bbox.append(bbox)
-        test_keypoints.append(keypoints)
-        test_label.append(label)
-    return test_image_arr, test_segmentation, test_bbox, test_keypoints, test_label
+testset = MiraBest(root='./batches', train=False, download=True, transform=transform) 
+batch_size_test = 2
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size_test, shuffle=True, num_workers=2)
